c1e8e3e8e9c9dfd18c33a6605ab059d66f671c92
Patrick O'Connor <poc.967@gmail.com>
2020-03-20 20:55:18 -0400

    further project core setup

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv').config()
 
 app.all('*', (request, response) => {
     response.sendStatus(404)
 })
 
-const server = app.listen(8080, () => {
-    console.log('Listening on port 8080...')
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}...`)
 })
 
-module.exports = server
+module.exports = { server }
