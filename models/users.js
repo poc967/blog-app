56e0b66c021a56d02bcfc4fe84274da1ef997b6a
Patrick O'Connor <poc.967@gmail.com>
2020-03-21 18:13:49 -0400

    added get and post route for users

---
+++ models/users.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    }
+})
+
+const User = mongoose.model('User', UserSchema)
+module.exports = User
