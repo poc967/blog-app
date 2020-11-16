bd99c1dd626a2d7a6fa3944d309b851b8154526f
Patrick O'Connor <poc.967@gmail.com>
2020-11-15 23:28:53 -0500

    fix

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(
 );
 app.use(bodyParser.json());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "/")));
 
 //Connection to MongoDB
 
