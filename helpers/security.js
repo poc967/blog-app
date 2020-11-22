b50ddd87003981ab0fb83c33cefba5125e92f1d8
Patrick O'Connor <poc.967@gmail.com>
2020-11-21 21:48:11 -0500

    fix

--- helpers/security.js
+++ helpers/security.js
@@ -55,7 +55,7 @@ const authenticateUser = async (request, response, next) => {
           async (err, token) => {
             if (err) throw err;
             return response
-              .cookie("token", token, { httpOnly: true })
+              .cookie("token", token, { httpOnly: true, sameSite: "none" })
               .status(200)
               .json(
                 await User.findOne({ _id: user._id })
