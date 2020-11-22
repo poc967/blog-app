7782425f15176247abe036d531a78f43aa5e0eea
Patrick O'Connor <poc.967@gmail.com>
2020-11-21 22:06:44 -0500

    fix

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ connection.on("error", (error) => {
 //-----------------------------------------------------
 
 const corsOptions = {
-  origin: "https://enigmatic-springs-23614.herokuapp.com/",
+  origin: "https://enigmatic-springs-23614.herokuapp.com",
   credentials: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: true,
